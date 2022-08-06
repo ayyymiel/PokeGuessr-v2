e7e5b2edcbebe2b4fc06c81fd50fe08e0395b80e
ayyymiel <gozonamiel@gmail.com>
2022-08-06 00:21:04 -0400

    small front-end stuff added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import './index.css';
 
-function App() {
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Typography from '@mui/material/Typography';
+
+const generalTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello World!</p>
-      </header>
-    </div>
+    <ThemeProvider theme={generalTheme}>
+      <CssBaseline />
+          <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                >
+                  <MenuIcon />
+              </IconButton>
+              <Typography variant="h5" 
+                component="div" 
+                align="center" 
+                sx={{flexGrow: 1}}>
+                  Poke-dle!
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+
+        <Box component="img" sx={{
+          height: 233,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+        }}
+        src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"> 
+
+        </Box>
+    </ThemeProvider>
   );
 }
-
-export default App;
